dacb5017a42f45ce5ddc46adcf4211d4601c07e6
sylvain.dechaumet@gmail.com <sylvain.dechaumet@gmail.com>
2023-12-18 16:39:17 +0000

    added test-00

--- tests/testthat/test-00-devonly.R
+++ tests/testthat/test-00-devonly.R
@@ -1,5 +1,15 @@
 testthat::skip("For development purpose only, do not run !")
 
 test_that("TODO", {
-  TRUE
+  ## Implementing:
+  # init_object:           OK
+  # add_events:            OK
+  # add_cpd_events:        TODO
+  # add_xics:              TODO
+  # add_best_peaks:        TODO
+  # add_events_to_extract: TODO
+  # add_spectra:           TODO
+  # add_mspurity:          TODO
+  # add_annot:             TODO
+  # export_tables:         TODO
 })
